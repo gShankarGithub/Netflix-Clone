74bd1df8240f1a57fde6d17331e0486adce56643
gShankarGithub <gokulsshankar.official@gmail.com>
2022-11-25 10:15:25 +0530

    Add existing file

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import NavBar from './Components/NavBar/NavBar'
+import { action, comedy, horror, originals, romance } from './url'
+import "./App.css"
+import Banner from './Components/Banner/Banner'
+import RowPost from './Components/RowPost/RowPost'
+function App() {
+
+  return (
+    <div className="App">
+      <NavBar/>
+      <Banner/>
+      <RowPost url={originals} title='Netflix Originals'/>
+      <RowPost url={action} title='Action' isSmall/>
+      <RowPost url={comedy} title='Comedy' isSmall/>
+      <RowPost url={horror} title='Horror' isSmall/>
+      <RowPost url={romance} title='Romance' isSmall/>
+
+
+    </div>
+  )
+}
+
+export default App
---
+++ src/constants/constants.js
@@ -0,0 +1,3 @@
+export const baseUrl = 'https://api.themoviedb.org/3'
+export const API_KEY = '9eb5fdfd2c023fd1c8f83c1aea453049'
+export const imageUrl = 'https://image.tmdb.org/t/p/original'
